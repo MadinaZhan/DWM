7de84ed5f334d9e56515e95ac8c5cd1d6009c3a7
MadinaZhan <madinok0109@gmail.com>
2021-10-01 20:32:23 +0300

    third commit

--- dance_with_madina/settings.py
+++ dance_with_madina/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dance_with_madina/static')
+    os.path.join(BASE_DIR, 'mainapp/static')
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Mainapp
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def index(request):
     dances=Mainapp.objects.all()
     return render(request,'index.html',{'dances':dances})
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегестрировались!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
